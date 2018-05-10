a0abb154d5a6479216da14f0a0b2f0b7cbef7341
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-05-10 13:58:10 +0200

    Updated path to static.

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname + '/dist/pwa-mat/')));
 var port = process.env.PORT || 5000;
 app.listen(port);
-app.use('/', express.static('public'))
+app.use('/', express.static(path.join(__dirname + '/dist/pwa-mat/')))
 app.route('/*')
     .get(function(req, res) {
           res.sendFile(path.join(__dirname + '/dist/pwa-mat/index.html'));
