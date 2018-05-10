39c805a6a6917fd1ac391a95362e9ad84c92819e
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-05-10 13:02:28 +0200

    Added app route config file.

--- dist/pwa-mat/server.js
+++ dist/pwa-mat/server.js
@@ -5,4 +5,11 @@ app = express();
 app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
 app.listen(port);
+app.use('/', express.static('public'))
+app.route('/*')
+    .get(function(req, res) {
+          res.sendFile(path.join(__dirname + '/dist/pwa-mat/index.html'));
+});
+
+module.exports = app;
 console.log('server started '+ port);
