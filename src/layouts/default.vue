ad6a0ec3af81b1e384f0258eb3c5e5df0f5220d4
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-06-29 15:21:54 +0200

    New colour for header.

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,17 +2,46 @@
   <q-layout view="lHh Lpr lFf">
     <q-layout-header>
       <q-toolbar
-        color="primary"
+        color="green"
         :glossy="$q.theme === 'mat'"
         :inverted="$q.theme === 'ios'"
       >
         <q-toolbar-title>
-          FarmHands
+          Welcome to FarmHands
         </q-toolbar-title>
       </q-toolbar>
+      <nav-tabs v-if="$q.theme === 'mat'" />
     </q-layout-header>
     <q-page-container>
+      <q-fab
+  color="purple"
+  icon="keyboard_arrow_up"
+  direction="up"
+>
+  <q-fab-action
+    color="primary"
+    @click="someMethod"
+    icon="mail"
+  />
+  <q-fab-action
+    color="secondary"
+    @click="someMethod"
+    icon="alarm"
+  />
+</q-fab>
       <router-view />
+      <template>
+  <!-- Round shaped (only use icon or spinner as content!) with loading state -->
+  <q-btn
+    :loading="loading"
+    round
+    icon="map"
+    color="green"
+    @click="handler"
+  >
+    <q-spinner-oval slot="loading" />
+  </q-btn>
+</template>
     </q-page-container>
     <q-layout-footer>
       <q-toolbar
--- src/layouts/landing.vue
+++ src/layouts/landing.vue
@@ -10,10 +10,38 @@
           FarmHands
         </q-toolbar-title>
       </q-toolbar>
+      <nav-tabs v-if="$q.theme === 'mat'" />
     </q-layout-header>
     <q-page-container>
+      <q-fab
+  color="purple"
+  icon="keyboard_arrow_up"
+  direction="up"
+>
+  <q-fab-action
+    color="primary"
+    @click="someMethod"
+    icon="mail"
+  />
+  <q-fab-action
+    color="secondary"
+    @click="someMethod"
+    icon="alarm"
+  />
+</q-fab>
       <router-view />
     </q-page-container>
+    <q-layout-footer>
+      <q-toolbar
+        color="primary"
+        :glossy="$q.theme === 'mat'"
+        :inverted="$q.theme === 'ios'"
+      >
+      <q-toolbar-title>
+         @Helmut Steiner
+      </q-toolbar-title>
+     </q-toolbar>
+    </q-layout-footer>
   </q-layout>
 </template>
 
