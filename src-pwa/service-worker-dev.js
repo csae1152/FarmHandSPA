368b4a519cfbbd98652a16102ea655266f94823d
Helmut Steiner <helmut.steinersti2@gmail.com>
2018-09-16 11:36:56 +0200

    Update service-worker-dev.js

--- src-pwa/service-worker-dev.js
+++ src-pwa/service-worker-dev.js
@@ -4,7 +4,7 @@
 // file that will precache your site's local assets.
 // See https://github.com/facebookincubator/create-react-app/issues/2272#issuecomment-302832432
 
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('update', () => self.skipWaiting());
 
 self.addEventListener('activate', () => {
   self.clients.matchAll({ type: 'window' }).then(windowClients => {
