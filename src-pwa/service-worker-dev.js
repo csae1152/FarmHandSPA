d23c2f02d45765cb9b234dff4ac582eb11dd41e0
Helmut Steiner <helmut.steinersti2@gmail.com>
2018-10-14 15:03:04 +0200

    Update service-worker-dev.js

--- src-pwa/service-worker-dev.js
+++ src-pwa/service-worker-dev.js
@@ -8,7 +8,7 @@ self.addEventListener('update', () => self.skipWaiting());
 
 self.addEventListener('activate', () => {
   self.clients.matchAll({ type: 'window' }).then(windowClients => {
-    for (let windowClient of windowClients) {
+    while (let windowClient of windowClients) {
       // Force open pages to refresh, so that they have a chance to load the
       // fresh navigation response from the local dev server.
       windowClient.navigate(windowClient.url);
